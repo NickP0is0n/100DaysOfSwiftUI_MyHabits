47d2925e8c18f27fe4f70e1e88e05fc77c3e6dbb
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-30 15:28:35 +0300

    Initial Commit

---
+++ MyHabits/MyHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitsApp.swift
+//  MyHabits
+//
+//  Created by Mykola Chaikovskyi on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
